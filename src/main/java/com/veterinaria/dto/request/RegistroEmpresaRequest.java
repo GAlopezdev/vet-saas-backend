4c87afa392f4b43e1873a92b445ee6d187b7bf0f
Gabriel <i202332157@cibertec.edu.pe>
2026-02-06 23:08:07 -0500

    refactor: nuevas validaciones para Registrar Empresa Dto

--- src/main/java/com/veterinaria/dto/request/RegistroEmpresaRequest.java
+++ src/main/java/com/veterinaria/dto/request/RegistroEmpresaRequest.java
@@ -1,33 +1,46 @@
 package com.veterinaria.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record RegistroEmpresaRequest(
-        @NotBlank
-        @Email
+        @NotBlank(message = "El correo es obligatorio")
+        @Email(message = "El formato del correo es inválido")
         String correo,
-        @NotBlank
-        @Size(min = 8)
+
+        @NotBlank(message = "La contraseña es obligatoria")
+        @Size(min = 8, max = 64, message = "La contraseña debe tener entre 8 y 64 caracteres")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&.#_-])[A-Za-z\\d@$!%*?&.#_-]{8,64}$",
+                message = "La contraseña debe contener mayúsculas, minúsculas, números y caracteres especiales"
+        )
         String contrasenia,
-        @NotNull
+
+                @NotNull(message = "El tipo de empresa es obligatorio")
         Long idTipoEmpresa,
-        @NotBlank
+
+        @NotBlank(message = "El nombre comercial es obligatorio")
         String nombreComercial,
-        @NotBlank
+
+        @NotBlank(message = "La descripción es obligatoria")
+        @Size(max = 500, message = "La descripción no puede exceder los 500 caracteres")
         String descripcion,
-        @NotBlank
+
+        @NotBlank(message = "El RUC es obligatorio")
+        @Pattern(regexp = "\\d{11}", message = "El RUC debe tener 11 dígitos numéricos")
         String ruc,
-        @NotBlank
+
+        @NotBlank(message = "La razón social es obligatoria")
         String razonSocial,
-        @NotBlank
+
+        @NotBlank(message = "El teléfono es obligatorio")
         String telefono,
-        @NotBlank
+
+        @NotBlank(message = "El país es obligatorio")
         String pais,
-        @NotBlank
+
+        @NotBlank(message = "La ciudad es obligatoria")
         String ciudad,
-        @NotBlank
+
+        @NotBlank(message = "La dirección es obligatoria")
         String direccion
 ) {}
